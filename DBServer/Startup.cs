abc816b03850c8cb4f4d08a316c97b96f9dba1c1
R0b1S <robiss394@gmail.com>
2018-12-07 10:52:01 +0100

    added controllers

--- DBServer/Startup.cs
+++ DBServer/Startup.cs
@@ -26,6 +26,10 @@ namespace DBServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        
+            services.AddEntityFrameworkNpgsql().AddDbContext<PostgreSQLDBContext>(options =>
+                 options.UseNpgsql(Configuration.GetConnectionString("PostgreSQLConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
