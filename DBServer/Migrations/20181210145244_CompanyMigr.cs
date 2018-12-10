3d61a36b62c77f427d379fdac36b907571dca721
TheScriptan <skominas.ainis@gmail.com>
2018-12-10 15:55:14 +0100

    Set up DBServer

---
+++ DBServer/Migrations/20181210145244_CompanyMigr.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DBServer.Migrations
+{
+    public partial class CompanyMigr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
