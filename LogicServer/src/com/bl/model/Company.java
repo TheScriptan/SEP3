9a3cb1acfb234f688ac5c63e3d186351c5c4ef40
MihaiBarbus <32672404+MihaiBarbus@users.noreply.github.com>
2018-12-11 11:40:11 +0100

    Update Company.java
    added verify method

--- LogicServer/src/com/bl/model/Company.java
+++ LogicServer/src/com/bl/model/Company.java
@@ -21,23 +21,71 @@ public class Company {
 	public void setCompanyID(long companyID) {
 		this.companyID = companyID;
 	}
+	public boolean verifyCompanyID(long companyID)
+	{
+   	companyID = companyID.trim();
+   	if(companyID == null || companyID.equals(""))
+        return false;
+    	if(!companyID.matches("[0-9]*"))
+        return false;
+   	return true;
+	}
+	
 	public String getCompanyName() {
 		return companyName;
 	}
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+	public boolean verifyCompanyName(String companyName)
+	{
+    	companyName = companyName.trim();
+
+    	if(companyName == null || companyName.equals(""))
+        return false;
+    	if(!companyName.matches("[a-zA-Z0-9]*"))
+        return false;
+
+   	 return true;
+	}
+	
 	public String getCompanyAddress() {
 		return companyAddress;
 	}
 	public void setCompanyAddress(String companyAddress) {
 		this.companyAddress = companyAddress;
 	}
+	public boolean verifyCompanyAdress(String companyAdress)
+	{
+    	companyAdress = companyAdress.trim();
+
+    	if(companyAdress == null || companyAdress.equals(""))
+        return false;
+
+    	if(!companyAdress.matches("[a-zA-Z]*"))
+        return false;
+
+   	 return true;
+	}
+	
+	
 	public String getCompanyPhone() {
 		return companyPhone;
 	}
 	public void setCompanyPhone(String companyPhone) {
 		this.companyPhone = companyPhone;
+	}
+	private boolean verifyCompanyPhone(long companyPhone)
+	{
+   	 companyPhone = companyPhone.trim();
+
+   	 if(companyPhone == null || companyPhone.equals(""))
+        return false;
+
+    	if(!companyPhone.matches("[0-9]*"))
+        return false;
+
+   	 return true;
 	}
 	public String getCompanyEmail() {
 		return companyEmail;
@@ -45,7 +93,25 @@ public class Company {
 	public void setCompanyEmail(String companyEmail) {
 		this.companyEmail = companyEmail;
 	}
+	public boolean verifyCompanyEmail(String companyEmail)
+	{
+   	 companyEmail = companyEmail.trim();
 	
+    	if(companyEmail == null || companyEmail.equals(""))
+        return false;
+
+    	if(!companyEmail.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"))
+        return false;
+
+    	return true;
+	}
+	public boolean verify()
+	{
+		if(!(verifyCompanyName() && verifyCompanyName() && verifyCompanyAdress() && verifyCompanyPhone() && verifyCompanyEmail()))
+			return false;
+		return true;
+		
+	}	
 	@Override
 	public String toString() {
 		return "Company [companyID=" + companyID + ", companyName=" + companyName + ", companyAddress=" + companyAddress
