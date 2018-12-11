2e5c6c37edf30c24664c054fe97795a6bc827798
MihaiBarbus <32672404+MihaiBarbus@users.noreply.github.com>
2018-12-11 13:40:29 +0100

    Update CompleteShift.cs
    final version

--- DBServer/Models/CompleteShift.cs
+++ DBServer/Models/CompleteShift.cs
@@ -4,21 +4,21 @@ using System.ComponentModel.DataAnnotations;
 namespace DBServer.Models {
     public class CompleteShift {
         public CompleteShift () { }
-        public CompleteShift (long arg1, long arg2, long arg3, long arg4, DateTime arg5, DateTime arg6) {
-            this.ShiftDoneId = arg1;
-            this.ShiftId = arg2;
-            this.CompanyId = arg3;
-            this.StudentId = arg4;
+        public CompleteShift (long arg1, long arg2, long arg3, string arg4, DateTime arg5, DateTime arg6) {
+            this.ShiftDoneID = arg1;
+            this.ShiftID = arg2;
+            this.CompanyID = arg3;
+            this.StudentID = arg4;
             this.checkIn = arg5;
             this.checkIn = arg6;
 
         }
 
         [Key]
-        public long ShiftDoneId { get; set; }
-        public long ShiftId { get; set; }
-        public long CompanyId { get; set; }
-        public long StudentId { get; set; }
+        public long ShiftDoneID { get; set; }
+        public long ShiftID { get; set; }
+        public long CompanyID { get; set; }
+        public string StudentID { get; set; }
         public DateTime checkIn { get; set; }
         public DateTime checkOut { get; set; }
 
