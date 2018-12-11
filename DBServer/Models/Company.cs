da8aab8e5e1044513deff2159b7646a666a7fb60
MihaiBarbus <32672404+MihaiBarbus@users.noreply.github.com>
2018-12-11 13:30:09 +0100

    Update Company.cs
    final version

--- DBServer/Models/Company.cs
+++ DBServer/Models/Company.cs
@@ -2,17 +2,17 @@ using System;
 namespace DBServer.Models {
     public class Company {
         public Company () { }
-        public Company (long arg1, string arg2, string arg3, int arg4, string arg5) {
-            this.CompanyId = arg1;
+        public Company (long arg1, string arg2, string arg3, string arg4, string arg5) {
+            this.CompanyID = arg1;
             this.CompanyName = arg2;
             this.CompanyAdress = arg3;
             this.CompanyPhone = arg4;
             this.CompanyEmail = arg5;
         }
-        public long CompanyId { get; set; }
+        public long CompanyID { get; set; }
         public string CompanyName { get; set; }
         public string CompanyAdress { get; set; }
-        public int CompanyPhone { get; set; }
+        public string CompanyPhone { get; set; }
         public string CompanyEmail { get; set; }
 
     }
